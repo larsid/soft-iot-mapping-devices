4e1e9b2ac648240514a57b25efa65bb6aa87d46e
AllanCapistrano <allan.capistrano3@gmail.com>
2022-10-15 12:03:00 -0300

    Fix: updateProperties method

--- src/main/java/br/uefs/larsid/extended/mapping/devices/model/PIDEditor.java
+++ src/main/java/br/uefs/larsid/extended/mapping/devices/model/PIDEditor.java
@@ -15,77 +15,94 @@ import org.osgi.service.cm.ConfigurationAdmin;
 
 public class PIDEditor implements IPIDEditor {
 
-    private ConfigurationAdmin configurationAdmin;
+  private ConfigurationAdmin configurationAdmin;
 
-    public void setConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
-        this.configurationAdmin = configurationAdmin;
+  public void setConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
+    this.configurationAdmin = configurationAdmin;
+  }
+
+  @Override
+  public Optional<Map<String, Object>> getProperties(String pid)
+    throws IOException {
+    Dictionary<String, Object> properties =
+      this.configurationAdmin.getConfiguration(pid).getProperties();
+    return Optional.of(
+      Collections
+        .list(properties.keys())
+        .stream()
+        .collect(Collectors.toMap(Function.identity(), properties::get))
+    );
+  }
+
+  @Override
+  public Optional<Object> getProperty(String pid, String key)
+    throws IOException {
+    Optional<Map<String, Object>> optProperties = this.getProperties(pid);
+    if (optProperties.isPresent()) {
+      Map<String, Object> properties = optProperties.get();
+      Object property = properties.get(key);
+      return property == null ? Optional.empty() : Optional.of(property);
+    } else {
+      return Optional.empty();
     }
+  }
 
-    @Override
-    public Optional<Map<String, Object>> getProperties(String pid) throws IOException {
-        Dictionary<String, Object> properties = this.configurationAdmin.getConfiguration(pid).getProperties();
-        return Optional.of(Collections.list(properties.keys())
-                .stream()
-                .collect(Collectors.toMap(Function.identity(), properties::get)));
+  @Override
+  public void createPIDFile(String pid) throws IOException {
+    this.createPIDFile(pid, new HashMap());
+  }
+
+  @Override
+  public void createPIDFile(String pid, Map<String, Object> properties)
+    throws IOException {
+    Configuration configuration =
+      this.configurationAdmin.createFactoryConfiguration(pid);
+    configuration.update(this.convertToHashtable(properties));
+  }
+
+  @Override
+  public void deletePIDFile(String pid) throws IOException {
+    this.configurationAdmin.getConfiguration(pid).delete();
+  }
+
+  @Override
+  public void updateProperties(String pid, Map<String, Object> properties)
+    throws IOException {
+    for (Map.Entry<String, Object> entry : properties.entrySet()) {
+      this.updateProperty(pid, entry.getKey(), entry.getValue());
     }
+  }
 
-    @Override
-    public Optional<Object> getProperty(String pid, String key) throws IOException {
-        Optional<Map<String, Object>> optProperties = this.getProperties(pid);
-        if (optProperties.isPresent()) {
-            Map<String, Object> properties = optProperties.get();
-            Object property = properties.get(key);
-            return property == null ? Optional.empty() : Optional.of(property);
-        } else {
-            return Optional.empty();
+  @Override
+  public void updateProperty(String pid, String key, Object property)
+    throws IOException {
+    Configuration configuration = this.configurationAdmin.getConfiguration(pid);
+    Dictionary<String, Object> properties = configuration.getProperties();
+    properties.remove(key);
+    properties.put(key, property);
+    configuration.update(properties);
+  }
+
+  private Hashtable<String, Object> convertToHashtable(
+    Map<String, Object> properties
+  ) {
+    Hashtable<String, Object> hashtable = new Hashtable();
+    if (!properties.isEmpty()) {
+      hashtable.putAll(properties);
+    }
+    return hashtable;
+  }
+
+  private void listProperties(String pid) throws IOException {
+    this.getProperties(pid)
+      .ifPresent(
+        map -> {
+          map.forEach(
+            (k, v) -> {
+              System.out.println(k + " = " + v);
+            }
+          );
         }
-    }
-
-    @Override
-    public void createPIDFile(String pid) throws IOException {
-        this.createPIDFile(pid, new HashMap());
-    }
-
-    @Override
-    public void createPIDFile(String pid, Map<String, Object> properties) throws IOException {
-        Configuration configuration = this.configurationAdmin.createFactoryConfiguration(pid);
-        configuration.update(this.convertToHashtable(properties));
-    }
-
-    @Override
-    public void deletePIDFile(String pid) throws IOException {
-        this.configurationAdmin.getConfiguration(pid).delete();
-    }
-
-    @Override
-    public void updateProperties(String pid, Map<String, Object> properties) throws IOException {
-        Configuration configuration = this.configurationAdmin.getConfiguration(pid);
-        configuration.update(this.convertToHashtable(properties));
-    }
-
-    @Override
-    public void updateProperty(String pid, String key, Object property) throws IOException {
-        Configuration configuration = this.configurationAdmin.getConfiguration(pid);
-        Dictionary<String, Object> properties = configuration.getProperties();
-        properties.remove(key);
-        properties.put(key, property);
-        configuration.update(properties);
-    }
-
-    private Hashtable<String, Object> convertToHashtable(Map<String, Object> properties) {
-        Hashtable<String, Object> hashtable = new Hashtable();
-        if (!properties.isEmpty()) {
-            hashtable.putAll(properties);
-        }
-        return hashtable;
-    }
-
-    private void listProperties(String pid) throws IOException {
-        this.getProperties(pid).ifPresent((map) -> {
-            map.forEach((k, v) -> {
-                System.out.println(k + " = " + v);
-            });
-        });
-    }
-
+      );
+  }
 }
